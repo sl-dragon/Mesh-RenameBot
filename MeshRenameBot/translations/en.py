b0b4094f01ef0e20853b360eb9ad7920bfc9584e
sl-dragon <85823565+sl-dragon@users.noreply.github.com>
2022-01-11 10:53:38 +0530

    Update en.py

--- MeshRenameBot/translations/en.py
+++ MeshRenameBot/translations/en.py
@@ -2,7 +2,7 @@ class EnTrans:
     
     WRONG_VALUE_ERROR = "Invalid value entered for {} variable."
     
-    START_MSG = "Hello there, I am Mesh Rename Bot. Created by yosh. You can deploy your own."
+    START_MSG = "Hello there, I am Mesh Rename Bot. Created by yosh. "
      
     CANCEL_MESSAGE = "The rename has been canceled. Will be updated soon."
     
